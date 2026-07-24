ecaeec953511d347f3bbdecf98a98850ffff72d5
HouYunFei <1844025705@qq.com>
2026-07-24 19:06:37 +0800

    fix: read app version from package.json for live reload

--- web/src/constant/env.ts
+++ web/src/constant/env.ts
@@ -1,1 +1,3 @@
-export const APP_VERSION = __APP_VERSION__ || "0.1.2";
+import { version as packageVersion } from "../../package.json";
+
+export const APP_VERSION = packageVersion || __APP_VERSION__ || "0.1.2";
